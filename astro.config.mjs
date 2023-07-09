a8352708282116e27987ef95008a0c511cfc5543
Juan Castillo <cfuendesign@tutanota.com>
2023-07-09 17:33:11 -0500

    landing page mostly done

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import alpinejs from "@astrojs/alpinejs";
+import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap(), alpinejs()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    sitemap(),
+    alpinejs(),
+    lit(),
+  ],
 });
